b746a0e0350007bf9e12128eb2b9e7f9dff93aad
TheSilentCommit <klaus.kaz@proton.me>
2026-07-13 15:32:45 -0300

    chore: create services layer

--- utils/responses.utils.js
+++ utils/responses.utils.js
@@ -1,4 +1,4 @@
-export const sendMessage = (res, statusCode, message, success, data=null) => {
+export const sendMessage = (res, statusCode, message, success, data=[]) => {
     return res.status(statusCode).json({
         success,
         message,
